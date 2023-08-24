7169a78867352ce90ad6fb29277490b227ae8219
Ba <neon@pc>
2023-08-24 15:44:07 +0700

    Update operator type

--- src/MongoDBRealtime.ts
+++ src/MongoDBRealtime.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, ChangeStream, ChangeStreamDocument } from 'mongodb'
 import { LivequeryBaseEntity, DatabaseEvent } from '@livequery/types'
 import { Observable, from, lastValueFrom, toArray } from 'rxjs'
 
@@ -26,12 +26,26 @@ export const listenMongoDBDataChange = <T extends LivequeryBaseEntity = Livequer
                 })
                 .on('error', console.error)
                 .on('change', (change: any) => {
-                    o.next({
-                        table: change.ns.coll,
-                        type: change.operationType,
-                        new_data: change.fullDocument,
-                        old_data: change.fullDocumentBeforeChange
-                    })
+                    const types = {
+                        insert: 'added',
+                        update: 'modified',
+                        delete: 'removed'
+                    }
+
+                    if (types[change.operationType]) {
+                        const id = change.documentKey?._id?.toString()
+                        if (id) {
+                            return o.next({
+                                table: change.ns.coll,
+                                type: types[change.operationType],
+                                new_data: { ...change.fullDocument, _id: undefined, id },
+                                old_data: { ...change.fullDocumentBeforeChange, _id: undefined, id }
+                            })
+                        }
+                    }
+
+
+
                 })
 
             await new Promise<void>(s => {
